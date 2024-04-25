18f8c8ba7df6ebc56f2770aa26aa8d5c827309b5
Tran Duy Bim <tranduybim2906@gmail.com>
2024-04-25 09:35:30 +0700

    update

--- test.php
+++ test.php
@@ -1,38 +1,37 @@
 <?php
-    // $NgonNgu1 = "PHP";
-    // $NgonNgu2 = "ASP.NET";
-    // echo $NgonNgu1, " và " , $NgonNgu2, " là ngôn ngữ Webserver.";
+    $NgonNgu1 = "PHP";
+    $NgonNgu2 = "ASP.NET";
+    echo $NgonNgu1, " và " , $NgonNgu2, " là ngôn ngữ Webserver.";
 
-    // $num1 = 10;
-    // $num2 = 20;
-    // printf("%d + %d = %d", $num1, $num2, $num1 + $num2);
+    $num1 = 10;
+    $num2 = 20;
+    printf("%d + %d = %d", $num1, $num2, $num1 + $num2);
 
-    // define("PI", 3.14);
-    // $r = 10;
-    // $s = PI * pow($r,2);
-    // $p = 2 * PI * $r;
-    // echo $p;
+    define("PI", 3.14);
+    $r = 10;
+    $s = PI * pow($r,2);
+    $p = 2 * PI * $r;
+    echo $p;
 
-    // $n = 100;
-    // $arr1 = array($n);
-    // $arr2 = array(1, 2, 3);
-    // $arr3 = array();
-    // $arr4 = array('hoten' => "KKien",
-    //     'quequan' => "PH",
-    //     'tuoi' => 24,
-    //     'IQ' => "Rất cao");
+    $n = 100;
+    $arr1 = array($n);
+    $arr2 = array(1, 2, 3);
+    $arr3 = array();
+    $arr4 = array('hoten' => "KKien",
+        'quequan' => "PH",
+        'tuoi' => 24,
+        'IQ' => "Rất cao");
 
-    // $var = "100" + 15;
-    // echo $var;
-    // echo "<br>";
-    // $var = "100" + 15.0;
-    // echo $var;
-    // echo "<br>";
-    // $var = 39 . "Steps";
-    // echo $var;
+    $var = "100" + 15;
+    echo $var;
+    echo "<br>";
+    $var = "100" + 15.0;
+    echo $var;
+    echo "<br>";
+    $var = 39 . "Steps";
+    echo $var;
 
-    // $a = 10;
-    // $b = &$a;
-    // echo "$a<br>$b";
-
+    $a = 10;
+    $b = &$a;
+    echo "$a<br>$b";
 ?>
