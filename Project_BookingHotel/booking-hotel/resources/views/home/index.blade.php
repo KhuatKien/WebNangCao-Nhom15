e4ad146d915d55aec008668969e9c25b374e8383
TranDuyBim <tranduybim2906@gmail.com>
2024-06-12 16:34:40 +0700

    .

--- Project_BookingHotel/booking-hotel/resources/views/home/index.blade.php
+++ Project_BookingHotel/booking-hotel/resources/views/home/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('home.css') }}">
+    <link rel="stylesheet" href="{{ asset('layouts.css') }}">
     <script src="https://kit.fontawesome.com/7b9d8c4ddc.js" crossorigin="anonymous"></script>
     <title>Home</title>
 </head>
