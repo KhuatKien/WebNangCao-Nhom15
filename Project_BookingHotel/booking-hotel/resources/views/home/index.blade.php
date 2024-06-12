f4b8c0c268bba1537be99181528050198e5d35f8
KhuatKien <kien18122k3@gmail.com>
2024-06-12 16:12:33 +0700

    update layouts

--- Project_BookingHotel/booking-hotel/resources/views/home/index.blade.php
+++ Project_BookingHotel/booking-hotel/resources/views/home/index.blade.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('home.css') }}">
+    <link rel="stylesheet" href="{{ asset('layouts.css') }}">
     <script src="https://kit.fontawesome.com/7b9d8c4ddc.js" crossorigin="anonymous"></script>
     <title>Home</title>
 </head>
@@ -108,4 +108,10 @@
     
 </body>
 
-</html>
+</html> --}}
+
+@extends('layouts.app')
+
+@section('content')
+    
+@endsection
---
+++ Project_BookingHotel/booking-hotel/resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('layouts.css') }}">
+    <script src="https://kit.fontawesome.com/7b9d8c4ddc.js" crossorigin="anonymous"></script>
+    <title>Home</title>
+</head>
+
+@extends('layouts.header')
+
+@yield('content')
+
+@extends('layouts.footer')
